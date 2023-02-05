dfeba411979e87e255c5c8bab40149737bef2007
DixitAnjali01 <122504032+DixitAnjali01@users.noreply.github.com>
2023-02-05 15:51:12 +0530

    Add files via upload

---
+++ bcp6.py
@@ -0,0 +1,5 @@
+#PROGRAM TO INPUT TWO INTEGER VALUES AND CALCULATE FIRST NUMBER RAISED TO THE POWER OF SECOND NUMBER
+num1=int(input("Enter first integer:"))
+num2=int(input("Enter second integer:"))
+ans=num1**num2
+print("FIRST NUMBER RAISED TO THE POWER OF SECOND NUMBER IS = ",ans)
