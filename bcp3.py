878ea1e69105b40f68e3618b1447e8695c20b47b
DixitAnjali01 <122504032+DixitAnjali01@users.noreply.github.com>
2023-02-05 15:49:47 +0530

    Add files via upload

---
+++ bcp3.py
@@ -0,0 +1,5 @@
+#PROGRAM TO SUM TWO ENTERED INTEGER VALUES
+num1=int(input("Enter first integer:"))
+num2=int(input("Enter second integer:"))
+sum=num1+num2
+print("The sum of two integers is = ",sum)
