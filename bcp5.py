fe53e142a9b5bf7487a4271b9b05dc973135d092
DixitAnjali01 <122504032+DixitAnjali01@users.noreply.github.com>
2023-02-05 15:50:45 +0530

    Add files via upload

---
+++ bcp5.py
@@ -0,0 +1,5 @@
+#PROGRAM TO MULTIPLY TWO ENTERED INTEGER VALUES
+num1=int(input("Enter first integer:"))
+num2=int(input("Enter second integer:"))
+product=num1*num2
+print("The sum of two integers is = ",product)
