0b6678d76d87bb6bb9d3e646feb57d087e9014bd
DixitAnjali01 <122504032+DixitAnjali01@users.noreply.github.com>
2023-02-05 15:51:49 +0530

    Add files via upload

---
+++ bcp8.py
@@ -0,0 +1,6 @@
+#PROGRAM TO FIND AREA AND CIRCUMFERENCE OF THE CIRCLE
+r=float(input("Enter the radius:"))
+area=3.14*(r**2)
+circum=2*3.14*r
+print("The area of the circle is :",area)
+print("The circumference of the circle is :",circum)
