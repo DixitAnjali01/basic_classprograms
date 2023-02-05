9ecd635bcd0946ba99f958911d3e415c94d0c2ba
DixitAnjali01 <122504032+DixitAnjali01@users.noreply.github.com>
2023-02-05 15:52:26 +0530

    Add files via upload

---
+++ bcp10.py
@@ -0,0 +1,7 @@
+#PROGRAM TO SWAP TWO VARIABLES'VALUES
+x=int(input("Enter the value of first variable(x):"))
+y=int(input("Enter the value of second variable(y):"))
+print("Before swapping value of x is %d and y is %d "%(x,y))
+x,y=y,x
+print("After swapping value of x is %d and y is %d "%(x,y))
+
