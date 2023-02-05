92f51fd8a60e073d6b7d10d080db8305e3e1692c
DixitAnjali01 <122504032+DixitAnjali01@users.noreply.github.com>
2023-02-05 15:51:30 +0530

    Add files via upload

---
+++ bcp7.py
@@ -0,0 +1,7 @@
+#PROGRAM TO FIND AREA AND PERIMETER OF THE RECTANGLE
+l=float(input("Enter the length:"))
+b=float(input("Enter the breadth:"))
+area=l*b
+peri=2*(l+b)
+print("The area of the rectangle is :",area)
+print("The perimeter of the rectangle is :",peri)
